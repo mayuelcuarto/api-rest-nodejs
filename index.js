29ad8c7d262692899c905a2cc42af0ebc4d556a8
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-04-02 04:58:42 -0500

    Conexion a mongodb con mongoose

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Test");
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/portafolio')
+        .then(() => {
+            console.log("Conexión a la base de datos establecida satisfactoriamente...");
+        }).
+        catch(err => console.log(err));
