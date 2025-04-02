468af313a0b11f2ec715e009ac42f9c37978838e
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-04-02 05:13:05 -0500

    Configuracion de servidor y rutas de prueba

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// cargar archivos de rutas
+
+// middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// CORS
+
+// rutas
+app.get('/', (req, res) => {
+    res.status(200).send(
+        "<h1>Página de inicio</h1>"
+    );
+});
+
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        message: "Hola mundo desde mi API de NodeJS"
+    });
+});
+
+// exportar
+module.exports = app;
